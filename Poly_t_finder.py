80e3b3525d2a42890a7179169d2607ca26237009
Ram Kumar R S <39699070+RSRamKumar@users.noreply.github.com>
2020-04-15 17:29:00 +0530

    Update Poly_t_finder.py
    This code identifies poly-t sequence when an input containing start,stop,len,sequence is given

--- Poly_t_finder.py
+++ Poly_t_finder.py
@@ -22,11 +22,17 @@ for i in X_1:
     f2.write('{},{},{},{}\n'.format(a,b,c,d))
     
     
-# This code identifies poly-t sequence when an input containing start,stop,len,sequence is given####
-The sample input is shown, the output is written fetching only the rows having poly t in the tail sequence the condition is mentioned above
+# This code identifies poly-t sequence when an input containing start,stop,len,sequence is given. ####
+#The sample input is shown, the output is written fetching only the rows having poly t in the tail sequence the condition is mentioned above
 #5,154,786,atactagatactagaataatactattactttttttttttttacttttttactttttttttttttttttttttttt
 #15,154,786,atactagatactagaataatactattacttgatttt
 #25,154,786,atactagatactagaataatactattac
 #30,154,786,atactagatactagaataatactattactatatactagactaactactactactacatagatacaatttt
 #40,154,786,atactagatactagaataa
 #45,154,786,atactagatactagaataatactatt
+
+######### Different other versions of presence of poly-t in a sequence####
+# m=re.findall(r'[t{4,}[actg]+]',line)
+#m=re.findall(r'[t{4,}[actg]{4} | [actg]{4}t{4,}$]',line)
+#m=re.findall(r'[actg]+t{4,}',r)
+#i=re.findall(r't{4,}[actg]{10}',r)
